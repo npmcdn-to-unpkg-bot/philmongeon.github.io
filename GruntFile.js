86ebb3ee5d11bae1373974409381ff3104d315be
Phil Mongeon <phil.mongeon@gmail.com>
2014-08-02 12:10:44 -0400

    Initial

---
+++ GruntFile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+
+        uglify: {
+            build: {
+                options: {
+                    mangle: true,
+                    beautify: false,
+                },
+                files: {
+                    'scripts/min/main.min.js':
+                        [
+                            'scripts/vendor/modernizr.js',
+                            'scripts/vendor/respond.js',
+                            'scripts/main.js'
+                        ]
+                }
+            }
+        },
+
+        less: {
+          build: {
+            options: {
+                paths: ['less'],
+                compress: false,
+                cleancss: false
+            },
+            files: {
+                'styles/min/main.min.css': 'styles/main.less'
+            }
+          }
+        },
+
+        watch: {
+            scripts: {
+                files: ['scripts/*.js'],
+                tasks: ['uglify']
+            },
+            styles: {
+                files: ['styles/*.less'],
+                tasks: ['less']
+            }
+        }
+    
+    });
+
+};
