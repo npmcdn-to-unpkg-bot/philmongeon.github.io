9f2d9ffcf7150749615070bdab4426497fe3aac4
Phil Mongeon <phil.mongeon@gmail.com>
2014-08-07 18:29:05 -0400

    cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,45 @@
 var Philz = window.Philz || {};
 
-Philz.els = {
-  window:   $(window),
-  body:     $('body'),
-  child:    $('.description'),
-  fade_in:  $('.fade-in'),
-  trans_in: $('.translate-in')
-};
+Philz = {
+    
+  els: {
+      window:   $(window),
+      body:     $('body'),
+      child:    $('.description'),
+      fade_in:  $('.fade-in'),
+      trans_in: $('.translate-in')
+  },
 
-Philz.animate = function() {
-    Philz.els.fade_in.addClass('faded');
-    Philz.els.trans_in.addClass('translated');
-};
+  animate: function() {
+    this.els.fade_in.addClass('faded');
+    this.els.trans_in.addClass('translated');
+  },
 
-Philz.centerChild = function() {
-  Philz.els.child.each(function() {
-    var container, margin_top, section;
-    container = $(this);
-    section = container.parent();
-    margin_top = ( (section.height() - container.height()) / 2 ) - 10;
-    container.css('margin-top', margin_top);
-  });
-};
+  centerChild: function() {
+    this.els.child.each(function() {
+      var container, margin_top, section;
+      container = $(this);
+      section = container.parent();
+      margin_top = ( (section.height() - container.height()) / 2 ) - 10;
+      container.css('margin-top', margin_top);
+    });
+  },
 
-Philz.els.window.on({
-  load: function() {
-    Philz.els.body.addClass('loaded');
-    Philz.els.window.trigger('resize');
-    Philz.animate();
+  init: function() {
+    this.els.window.on({
+      load: function() {
+        Philz.els.body.addClass('loaded');
+        Philz.els.window.trigger('resize');
+        Philz.animate();
+      },
+      resize: function() {
+       Philz.centerChild();
+      }
+    });
   }
-});
 
-Philz.els.window.on({
-  resize: function() {
-    Philz.centerChild();
-  }
-});
+};
+
+(function(){
+  Philz.init();
+})();
