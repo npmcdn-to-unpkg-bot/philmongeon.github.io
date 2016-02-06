e4270d05cdcdbc8acaa754cf707c39f56d434fee
Phil Mongeon <philz@Phils-MacBook-Pro.local>
2016-02-06 01:47:51 -0500

    lol

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,18 @@
-var Philz = window.Philz || {};
+(function() {
+  
+  var animate = document.getElementsByClassName('animate');
+  var body = document.getElementsByTagName('body')[0];
 
-Philz = {
-    
-  els: {
-      window:   $(window),
-      body:     $('body'),
-      child:    $('.description'),
-      fade_in:  $('.fade-in'),
-      trans_in: $('.translate-in')
-  },
+  function init() {
+   
+    body.classList.add('loaded');
 
-  animate: function() {
-    this.els.fade_in.addClass('faded');
-    this.els.trans_in.addClass('translated');
-  },
+    for( var i = 0; i < animate.length; i++ ) {
+       animate[i].classList.add('faded','translated');
+    }
 
-  centerChild: function() {
-    this.els.child.each(function() {
-      var container = $(this),
-          section = container.parent(),
-          margin_top = ( (section.height() - container.height()) / 2 ) - 10;
-      container.css('margin-top', margin_top);
-    });
-  },
-
-  init: function() {
-    this.els.window.on({
-      load: function() {
-        Philz.els.body.addClass('loaded');
-        Philz.animate();
-        Philz.els.window.trigger('resize');
-      },
-      resize: function() {
-       Philz.centerChild();
-      }
-    });
   }
 
-};
+  window.addEventListener('load', init);
 
-Philz.init();
+})();
