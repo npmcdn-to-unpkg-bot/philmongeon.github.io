dd1bcd3052261572ce19e28970cf71f8dcefdc32
Phil Mongeon <philz@Phils-MacBook-Pro.local>
2016-07-22 10:55:23 -0400

    jp fire edits

--- assets/js/dist/main.js
+++ assets/js/dist/main.js
@@ -7,18 +7,43 @@ var randomColor = require('../../../bower_components/randomcolor/randomColor');
 
 		colors: function() {
 
+			var body = document.body;
 			var links = document.links;
-			var hue_list = ['red', 'orange', 'green', 'blue', 'purple', 'pink'];
-			var hue = hue_list[Math.floor(Math.random()*hue_list.length)];
-			var random_color = randomColor({
-				luminosity: 'dark',
-				hue: hue,
-				count: 20
-			});
+			var speed = 7500;
 
-			for(var i = 0; i < links.length; i++) {
-				links[i].style.color = random_color[i+1];
+			var luminosity_list = ['light', 'dark'];
+			var hue_list = ['red', 'orange', 'green', 'blue', 'purple', 'pink'];
+
+			// var luminosity = luminosity_list[Math.floor(Math.random()*luminosity_list.length)];
+			var luminosity = 'light';
+
+			function setColours(update_luminosity) {
+
+				var hue = hue_list[Math.floor(Math.random()*hue_list.length)];
+
+				if(update_luminosity) {
+					luminosity = luminosity_list[Math.floor(Math.random()*luminosity_list.length)];
+					body.className = '';
+					body.classList.add(luminosity);
+				}
+
+				var random_color = randomColor({
+					luminosity: luminosity,
+					hue: hue,
+					count: 20
+				});
+
+				for(var i = 0; i < links.length; i++) {
+					links[i].style.color = random_color[i];
+				}
+
 			}
+			
+			setColours(false);
+			
+			setInterval(function() {
+				setColours(false);
+			}, speed);
 
 		},
 
